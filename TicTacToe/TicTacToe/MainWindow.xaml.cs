07ca79e1e61008d3448e0c80710c9b2879c53cba
grubertmayor <vstarovoyt29@gmail.com>
2020-12-14 23:07:39 +0200

    commit

---
+++ TicTacToe/TicTacToe/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+namespace TicTacToe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        int turn=0;
+        int i, j;
+
+        public MainWindow(): base()
+        {
+            this.InitializeComponent();
+        }
+
+        
+
+        private void win(object btnContent)
+        {
+            if ((Button1.Content == btnContent & Button2.Content == btnContent &
+                 Button3.Content == btnContent)
+               | (Button1.Content == btnContent & Button4.Content == btnContent &
+                 Button7.Content == btnContent)
+               | (Button1.Content == btnContent & Button5.Content == btnContent &
+                 Button9.Content == btnContent)
+               | (Button2.Content == btnContent & Button5.Content == btnContent &
+                 Button8.Content == btnContent)
+               | (Button3.Content == btnContent & Button6.Content == btnContent &
+                 Button9.Content == btnContent)
+               | (Button4.Content == btnContent & Button5.Content == btnContent &
+                 Button6.Content == btnContent)
+               | (Button7.Content == btnContent & Button8.Content == btnContent &
+                 Button9.Content == btnContent)
+               | (Button3.Content == btnContent & Button5.Content == btnContent &
+                 Button7.Content == btnContent))
+            {
+                if (btnContent.ToString() == "O")
+                {
+
+                    MessageBox.Show("PLAYER O WINS");
+                    Label1.Content = ++i;
+                }
+                else if (btnContent.ToString() == "X")
+                {
+                    MessageBox.Show("PLAYER X WINS");
+                    Label2.Content = ++j;
+                }
+                disablebuttons();
+            }
+
+            else
+            {
+                foreach (Button btn in gridTable.Children)
+                {
+                    if (btn.IsEnabled == true)
+                        return;
+                }
+                MessageBox.Show("GAME OVER NO ONE WINS");
+            }
+        }
+        private void disablebuttons()
+        {
+            foreach (Button btn in gridTable.Children)
+            {
+                btn.IsEnabled = false;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            if (turn%2 == 1)
+            {
+                btn.Content = "O";
+                LabelTurn.Content = "X";
+            }
+            else
+            {
+                btn.Content = "X";
+                LabelTurn.Content = "O";
+            }
+            btn.IsEnabled = false;
+            win(btn.Content.ToString());
+            turn += 1;
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            WindowHelper helper = new WindowHelper();
+            helper.Show();
+        }
+
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Button btn in gridTable.Children)
+            {
+                btn.Content = "";
+                btn.IsEnabled = true;
+            }
+        }
+
+    }
+}
---
+++ TicTacToe/TicTacToe/WindowHelper.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Interaction logic for WindowHelper.xaml
+    /// </summary>
+    public partial class WindowHelper : Window
+    {
+        public WindowHelper()
+        {
+            InitializeComponent();
+        }
+    }
+}
